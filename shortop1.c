a864e4129592973f0aabce6405d6a70d0fa17715
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-10-30 11:19:51 +0200

    Update shortop1.c

--- shortop1.c
+++ shortop1.c
@@ -2,15 +2,15 @@
 
 int main(void)
 {
-	// what would be the output of the printf statements
+// what would be the output of the printf statements
 	
-	int a, b;
-	
-  a=b=4;
+int a, b;
+
+a=b=4;
     
-  printf("++a=%d\n", ++a);
-	printf("b++=%d\n", b++);
-	printf("a=%d, b=%d\n ", a, b);
+printf("++a=%d\n", ++a);
+printf("b++=%d\n", b++);
+printf("a=%d, b=%d\n ", a, b);
 	
-	return 0;    
+return 0;    
 }
