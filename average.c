3db4511b0e22b29d589652ae38f9958f23b4fb09
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-10-30 10:04:56 +0200

    Add files via upload

---
+++ average.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+
+// calculates the average of 3 int values
+int main(void)
+{
+	/* modifications TODO
+     1. Get the 3 values as input from the user 
+     2. Calculate the average of 3 random values. Use rand() function to get the three random values
+    */
+
+    int a, b, c;
+    float avg;
+
+    a = 5; 
+    b = 7; 
+    c = 4; 
+
+    avg= (a+b+c)/3.0;
+
+    printf("Ο μέσος όρος των αριθμών %d, %d και %d είναι: %4.3f\n", a, b, c, avg);
+		
+	return 0;    
+}
