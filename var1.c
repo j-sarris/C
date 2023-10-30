fae5c12832f440fb425d552c96641d83ed3ce2bc
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-10-30 09:39:22 +0200

    Create var1.c
    Δηλώστε τις κατάλληλες μεταβλητές

---
+++ var1.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main(void)
+{
+	// declare the appropriate variables here
+
+	
+	ranking = 1;
+	mesos_oros = 19.8;
+	grade = 'A';
+	
+	printf("Η κατάταξη του σπουδαστή στην τάξη είναι: %dος\n", ranking);
+	printf("Ο μέσος όρος της βαθμολογίας του είναι: %3.2f\n", mesos_oros);
+	printf("Ο χαρακτηρισμός της φοίτησής του είναι: %c\n", grade);
+	
+	return 0;    
+}
