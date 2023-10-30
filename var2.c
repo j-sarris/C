b3eb2bc78287e18aa5aeeb67b4dc3ee1dcabb5b7
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-10-30 10:22:03 +0200

    Create var2.c

---
+++ var2.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main(void)
+{
+	// what would be the output values of the variables
+	
+	int a=4, b;
+	float c=6.4, d;
+    
+  a = 5.3;
+  b=c/2;
+  d=a/2;
+
+	printf("Οι τιμές των μεταβλητών είναι: a=%d, b=%d ,c=%f, d=%f\n",a, b, c, d);
+
+	return 0;    
+}
