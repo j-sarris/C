52a9773bc3467b26d141595f79511f35e8547e85
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-10-30 10:53:23 +0200

    Create shortop1.c

---
+++ shortop1.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+
+int main(void)
+{
+	// what would be the output of the printf statements
+	
+	int a, b;
+	
+  a=b=4;
+    
+  printf("++a=%d\n", ++a);
+	printf("b++=%d\n", b++);
+	printf("a=%d, b=%d\n ", a, b);
+	
+	return 0;    
+}
