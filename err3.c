09457e8d79105f2dc577b70e0310dba77ea6c1e4
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-10-31 11:41:09 +0200

    Create err3.c

---
+++ err3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main (){
+
+int a, b;
+char ch;
+
+printf("Give a character: ");
+scanf("%d", &ch);
+
+printf("Give two integer numbers: ");
+scanf("%d %d", a, b);
+
+if (ch = 'A')
+    printf("Your input was %d, %d, and %c", a, b, ch);
+else
+    printf(You did not input A as the character);
+
+return 0;
+
+}
