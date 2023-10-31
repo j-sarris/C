67eebce1708ab3b9a9f52f1e8af1c369cc84653a
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-10-31 14:16:33 +0200

    Update var2.c

--- var2.c
+++ var2.c
@@ -2,16 +2,18 @@
 
 int main(void)
 {
-// what would be the output values of the variables
+// what would be the output values of the variables. Try to understand why.
 	
 int a=4, b;
-float c=6.4, d;
+float c=6.4, d, e, f;
     
 a = 5.3;
 b=c/2;
 d=a/2;
+e=7/2;
+f=5/10*100;
 
-printf("Οι τιμές των μεταβλητών είναι: a=%d, b=%d ,c=%f, d=%f\n",a, b, c, d);
+printf("Οι τιμές των μεταβλητών είναι: a=%d, b=%d ,c=%f, d=%f, e=%f, f=%f\n",a, b, c, d, e, f);
 
 return 0;    
 }
