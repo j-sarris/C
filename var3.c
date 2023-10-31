29d635cffbb6ab89ab3243ba04f95f7cbce897fe
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-10-31 13:04:23 +0200

    Create var3.c

---
+++ var3.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+int main(void)
+{
+// what would be the output of the printf statement. Can you correct the mistake?
+	
+int a=4, b=7;
+float c;
+    
+c = (a+b)/2;
+
+printf("Ο μέσος όρος είναι: %f",c);
+
+return 0;    
+}
