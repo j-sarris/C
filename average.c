06f4c038acfaef4d69df5eab379b0b806cdc0407
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-10-31 12:23:53 +0200

    Update average.c

--- average.c
+++ average.c
@@ -6,6 +6,7 @@ int main(void)
 /* modifications TODO
      1. Get the 3 values as input from the user 
      2. Calculate the average of 3 random values. Use rand() function to get the three random values
+     3. Calculate the average of three float values. Show the output with 2 decimal digits.
 */
 
     int a, b, c;
