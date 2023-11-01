ea1663f73668d6da987d97ccc2d2b839a0f79553
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-11-01 10:59:20 +0200

    Create if_logical.c

---
+++ if_logical.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+  int a = 10;
+  int b = -5;
+
+  if (a > 0 && b > 0) {   // Change to || and see the result 
+    printf("Positive\n");
+  }
+
+  return 0;
+}
