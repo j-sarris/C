cab1aa19bbb88e392f7b52955df5f9f732c03ec7
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-11-01 10:56:39 +0200

    Create if2.c

---
+++ if2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+  int grade = 90;
+  
+  // Write an if statement that checks if grade >= 60 is true and prints “Pass\n” if it is. Otherwise if grade < 60 it prints “Fail”
+  // Modify grade to be 55 and check the output of the program
+
+  return 0;
+  
+}
