a63224cb92f044e9ce03cec87b1a0be1ffe144f5
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-11-01 10:24:59 +0200

    Create if1.c

---
+++ if1.c
@@ -0,0 +1,16 @@
+// explain the operation of the following code
+
+#include <stdio.h>
+
+int main(){
+  int a, b;
+
+  printf("Give two integers:5");
+  scanf("%d %d", &a, &b);
+    
+  if (a>b)
+    printf("%d", a);
+  else
+    printf("%d", b);
+  return 0;
+}
