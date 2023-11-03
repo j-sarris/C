03549ccf6a3cc6f252bfea597b317978a9f0063d
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-11-03 11:50:21 +0200

    Create arr2.c

---
+++ arr2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+  int arr[] = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+  int arr2[20];
+    
+  // Loop through arr and print its elements one by one in seperate lines
+    
+  // Assign values to arr2 as arr2[0]=0, arr2[1]=5, arr2[2]=10, etc 
+
+  
+}
