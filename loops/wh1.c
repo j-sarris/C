9ab129a5f6e23f938bed9138fab632d83fd4dd04
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-11-09 13:47:56 +0200

    Create wh1.c

---
+++ loops/wh1.c
@@ -0,0 +1,16 @@
+// a simple while loop
+// try to include the decrement inside the printf statement
+// try to include the decrement inside the while logical check
+
+#include <stdio.h>
+
+int main(){
+
+ int a =5;
+
+ while (a>0){
+   printf("\na=%d", a);
+   a--;
+ }
+ return 0;
+}
