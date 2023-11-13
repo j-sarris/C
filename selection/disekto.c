4add7e62c5d499a7af797469d6bde624501b9cef
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-11-13 12:18:56 +0200

    Create disekto.c

---
+++ selection/disekto.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+  int year;
+
+	printf("Give year: ");
+	scanf("%d",&year);
+	
+	if(year%4 == 0)
+	{
+		if(year%100 == 0)
+		{
+			if(year%400 == 0)
+				printf("Disekto\n");
+			else
+				printf("Kanoniko\n");
+		}
+		else 
+		     printf("Disekto\n");
+	}
+	else 
+	{
+		printf("Kanoniko\n");
+	}
+
+	return 0;
+}
