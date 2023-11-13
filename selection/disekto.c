e1a5fce8e2e810e1496b0eb72e259ffa7009f3b3
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-11-13 12:23:23 +0200

    Update disekto.c

--- selection/disekto.c
+++ selection/disekto.c
@@ -8,21 +8,20 @@ int main(void)
   scanf("%d",&year);
 	
   if(year%4 == 0)
-	{
-		if(year%100 == 0)
-		{
-			if(year%400 == 0)
-				printf("Disekto\n");
-			else
+  {
+    if(year%100 == 0)
+    {
+      if(year%400 == 0)
+			  printf("Disekto\n");
+      else
 				printf("Kanoniko\n");
-		}
-		else 
-			printf("Disekto\n");
-	}
-	else 
-	{
-		printf("Kanoniko\n");
-	}
-
-	return 0;
+    }
+    else 
+		  printf("Disekto\n");
+  }
+  else 
+  {
+    printf("Kanoniko\n");
+  }
+  return 0;
 }
