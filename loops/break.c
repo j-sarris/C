07fb2e7a81af13328e0bafedf7e84f3b8f405bfb
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-11-15 09:15:36 +0200

    Create break.c

---
+++ loops/break.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+  int num = 1;
+  int num_entered = 0;
+
+  while (num_entered < 10) {
+    printf("Please enter a number: ");
+    scanf("%d", &num);
+
+    // Write the code so as if a negative number is given to break the while loop
+    
+    num_entered++;
+  }
+
+  // Test if the loop has ended and print message "No negative number given"
+  // Else print message about negative number breaking the loop
+  
+}
