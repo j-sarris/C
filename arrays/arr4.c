beb6f93a969f741db1729fb23f8d1ae42887bab8
Yannis <79635419+j-sarris@users.noreply.github.com>
2023-11-27 10:52:31 +0200

    Update arr4.c

--- arrays/arr4.c
+++ arrays/arr4.c
@@ -6,6 +6,7 @@ int main(){
 
   int array[100], i;
   int sum=0, even=0, odd=0; 
+  int min, max, pos_min=0, pos_max=0;
 
   srand(time(NULL));
 
@@ -13,6 +14,8 @@ int main(){
   for (i=0; i<100; i++)
     array[i]=rand();
 
+  min=max=array[0]; // initialize variables to store min and max elements
+    
   for (i=0; i<100; i++){
     sum+=array[i]; // calculate the sum of all elemens
         
@@ -20,9 +23,22 @@ int main(){
       odd++; // calculate number of odd elements
     else 
       even++; // calculate number of even elements
+
+    if (array[i]<min){
+      min=array[i]; // store min element of array
+      pos_min=i; // store the position of the minimum element 
+    }
+
+    if (array[i]>max){
+      max=array[i]; // store max element of array
+      pos_max=i; // store the position of the maximum element            
+    }
+        
   }
     
   printf("To athroisma olon ton stoixeion einai %d\n", sum);
-  printf("Yparxoyn %d monoi kai %d zygoi arithmoi ston pinaka", odd, even);
+  printf("Yparxoyn %d monoi kai %d zygoi arithmoi ston pinaka\n", odd, even);
+  printf("Elaxisto stoixeio to %d, sth thesi %d\n", min, pos_min);
+  printf("Megisto stoixeio to %d, sth thesi %d", max, pos_max);
   return 0;
 }
