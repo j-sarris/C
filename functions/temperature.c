477f6a031999c46380e60a2a860f4c9f5fe7d31b
Yannis <79635419+j-sarris@users.noreply.github.com>
2024-01-09 11:29:34 +0200

    Create temperature.c

---
+++ functions/temperature.c
@@ -0,0 +1,23 @@
+// Reads 10 Fahrenhei temperatures given by the user, 
+// converts them to Celcius and calculates the average temp
+#include <stdio.h>
+
+float Fahr_to_Celc(float temp);
+
+int main(){
+  float tempF, sumC=0;
+  int i;
+  
+  for (i=1;i<=10;i++){
+    printf("Give %d-th temperature: ", i);
+    scanf("%f", &tempF);
+    sumC+=Fahr_to_Celc(tempF);
+  }
+  printf("Average Celcius temp is %f degrees", sumC/10.0);
+  return 0;
+}
+
+float Fahr_to_Celc(float temp)
+{
+  return (temp-32)*5/9;
+}
