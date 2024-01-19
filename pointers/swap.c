cbbf3427b095748663b3570f9297651c020943d8
Yannis <79635419+j-sarris@users.noreply.github.com>
2024-01-19 09:38:35 +0200

    Create swap.c

---
+++ pointers/swap.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void poor_swap (int a, int b); // using variables to demonstrate that local function variables does not change main variable values
+void swap (int *first, int *second); // using pointers to show that the actual main variables values are changed
+
+int main()
+{
+  int a, b;
+
+  printf("Give value of first variable: ");
+  scanf("%d", &a);
+
+  printf("Give value of second variable: ");
+  scanf("%d", &b);
+
+  poor_swap(a, b);
+  printf("After poor_swap values in main are: a=%d b=%d\n", a, b);
+
+  swap(&a, &b);
+  printf("After swap values in main are: a=%d b=%d\n", a, b);
+
+  return 0;
+}
+
+void poor_swap(int a, int b)
+{
+  int temp;
+
+  temp=a;
+  a=b;
+  b=temp;
+
+  printf("Inside poor_swap values are: a=%d b=%d\n", a, b);
+}
+
+void swap(int *first, int *second)
+{
+  int temp;
+
+  temp=*first;
+  *first=*second;
+  *second=temp;
+
+  printf("Inside poor_swap values are: a=%d b=%d\n", *first, *second);
+}
